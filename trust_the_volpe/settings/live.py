cacb6663cc1bc2801dc61a3159293b30e7d8454e
Pete Graham <git@petegraham.co.uk>
2015-10-29 20:53:06 +0000

    Clean-up S3 urls

--- trust_the_volpe/settings/live.py
+++ trust_the_volpe/settings/live.py
@@ -17,3 +17,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'trust-the-volpe'
+AWS_QUERYSTRING_AUTH = False
